f9c0e2c029730fc8da7b5ee064b354d10cc39f05
Samuel Ueberle <s.ueberle@fu-berlin.de>
2020-02-09 01:01:05 +0100

    fix double declare

--- index.d.ts
+++ index.d.ts
@@ -1,9 +1,18 @@
-declare interface PS {
-
-    constructor()
-    connect(): number
-    setChannel(channel: 0 | 1, enabled: 0 | 1, coupling: 0 | 1, range: number) : number
-    setTrigger(channel: 0| 1, adc: number, direction: 0 | 1, threshold: number, delay: number): number
-    stream(cb: Function): boolean
-    templateIndex(): number
+type Channel = {
+    name: string;
+    dc: number;
+    direction: number;
+    range: number;
+    enabled: number;
+}
+declare interface PicoSope {
+    constructor();
+    connect(): number;
+    setChannel(channel: 0 | 1, enabled: 0 | 1, coupling: 0 | 1, range: number): number;
+    setTrigger(channel: 0 | 1, adc: number, direction: 0 | 1, threshold: number, delay: number): number;
+    stream(cb: Function): boolean;
+    templateIndex(): number;
+    stop(): void;
+    disconnect(): boolean;
+    getChannel(channel: number): Channel;
 }
